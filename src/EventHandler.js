5549c8a037bdc13c892bad58bcf5ca0ee2097729
lamsasa <dh85311@gmail.com>
2023-04-12 10:36:18 +0900

    04/12

---
+++ src/EventHandler.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const EventHandler = () => {
+    const [message, setMessage] = useState("Hello");
+    const chageMag = (e) =>{
+
+    }
+    
+    return(
+        <>
+        </>
+    )
+}
+export default EventHandler;
